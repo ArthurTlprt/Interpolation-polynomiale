37d3b8e5669e2f4ee8187887ec12ef9de2da156f
ArthurTlprt <arthur.talpaert@isen-lille.fr>
2016-09-28 17:38:23 +0200

    calcul des omegas dans newton

--- base_newton.m
+++ base_newton.m
@@ -1,23 +1,19 @@
 
 %[-2; -1; 1; 2], [10; 4; 6; 3], 1
 # P est l'image de la fonction
-function P = base_newton(x, y, t)
-  P = 0;
-  L = [];
-  # calcul des fonctions L(i)
-  for i = 1:size(x)(1)
-    L(i) = 1;
-     for j = 1:size(x)(1)
-       if j == i
-         #display('division par zero -> 1')
-       else
-         # Produit des fractions
-         L(i) = L(i) * (  (t-x(j)) / (x(i)-x(j))  );
-       end
-     end
-   end
-   # calcul de la somme des y(i)*L(i)
-   for i = 1:size(x)(1)
-     P = P + (y(i)*L(i));
-   end
+function image = base_newton(x, y, t)
+  % calcul des omegas
+  omega = [];
+  for k=1:length(x)
+    omega(k) = 1;
+    for i=1:k
+      omega(k) = omega(k) * (t - x(i));
+    end
+    disp('omega(',k,') = ', omega(k));
+    printf('omega(%d) = %d\n',k,omega(k))
+  end
+  % calcul des coefficients
+  alpha = [];
+  alpha(1) = y(1);
+
 endfunction
--- exe_inter.m
+++ exe_inter.m
@@ -1,6 +1,8 @@
 # 2.a)
 disp('Base Canonique');
 
+figure('position', [100, 100, 800, 500])    % postion + size
+
 subplot (2, 1, 1)
 plot_func(@base_vandermonde,[-2; -1; 1; 2], [10; 4; 6; 3], [-2,2]);
 
@@ -12,6 +14,7 @@ plot_func(@base_lagrange,[-2; -1; 1; 2], [10; 4; 6; 3], [-2,2]);
 # 2.c)
 disp('Base de Newton');
 
+base_newton([-2; -1; 1; 2], [10; 4; 6; 3], 2);
 
-
+title("Base canonique, Base de Lagrange, Base de Newton");
 pause(3); %pause du graphique
