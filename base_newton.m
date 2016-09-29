12ff7a34aeb13f5bb128ac1834c55059250095f1
ArthurTlprt <arthur.talpaert@isen-lille.fr>
2016-09-29 19:14:39 +0200

    calcul des alphas dans newton

--- base_newton.m
+++ base_newton.m
@@ -9,11 +9,29 @@ function image = base_newton(x, y, t)
     for i=1:k
       omega(k) = omega(k) * (t - x(i));
     end
-    disp('omega(',k,') = ', omega(k));
+    %disp('omega(',k,') = ', omega(k));
     printf('omega(%d) = %d\n',k,omega(k))
   end
   % calcul des coefficients
-  alpha = [];
-  alpha(1) = y(1);
+
+  P = zeros(length(x));
+  for k=1:length(x)
+    for col=1:k
+      P(k,col) = 1;
+      for i=1:col
+        if i-1 == 0
+          'lol'
+        else
+          %(x(k) - x(i-1))
+          P(k,col) = P(k,col) * (x(k) - x(i-1));
+        end
+      end
+    end
+  end
+  P
+  alpha = P \ y
+
+
+
 
 endfunction
